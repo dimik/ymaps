d082f4153ee04c393a04e87aaddfd0bd45844c18
dimik <dimik@yandex.ru>
2012-10-09 15:30:37 +0400

    add map to window for debug

--- examples/geoobject-editor/plugins/map.js
+++ examples/geoobject-editor/plugins/map.js
@@ -1,9 +1,9 @@
 define(['ready!ymaps'], function (ymaps) {
 
-    return new ymaps.Map('map', {
+    return (window.map = new ymaps.Map('map', {
         center: [55.755773, 37.617761],
         zoom: 10,
         behaviors: ['drag', 'scrollZoom']
-    });
+    }));
 
 });
