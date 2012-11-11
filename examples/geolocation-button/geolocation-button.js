967daf7d9b5f7eeac3c7cab0743d88c23f916e99
dimik <dimik@yandex.ru>
2012-11-11 15:34:55 +0400

    increase circle opacity

--- examples/geolocation-button/geolocation-button.js
+++ examples/geolocation-button/geolocation-button.js
@@ -173,7 +173,7 @@ ymaps.ready(function () {
                 if(circle) {
                     map.geoObjects.remove(circle);
                 }
-                this._circle = circle = new ymaps.Circle([location, accuracy], {}, { opacity: 0.3 });
+                this._circle = circle = new ymaps.Circle([location, accuracy], {}, { opacity: 0.6 });
                 map.geoObjects.add(circle);
             }
         },
