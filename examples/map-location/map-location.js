d372a9b00fa87e0842903b1d45c2ed7a5a528273
dimik <dimik@yandex.ru>
2012-12-14 21:13:12 +0400

    html5 history api

--- examples/map-location/map-location.js
+++ examples/map-location/map-location.js
@@ -1,52 +1,85 @@
-function MapLocation(state) {
-    this._state = state || {};
+function MapLocation(map, state) {
+    this._map = map;
+    this._state = state;
+    this._silent = false;
+
+    map.events.add(['boundschange', 'typechange'], this._onStateChange, this);
 }
 
-MapLocation.prototype.setMap = function (map) {
-    map.events[map == null && 'remove' || 'add'](['boundschange', 'typechange'], this._onStateChange, this);
-
-    return this;
-};
-
 MapLocation.prototype._onStateChange = function (e) {
-    e.get('target').events.fire('statechange', {
-        oldState: this,
-        newState: new MapLocation({
-            center: (e.get('newCenter') || this.get('center')).map(MapLocation.toCoords),
-            zoom: e.get('newZoom') >= 0 ? e.get('newZoom') : this.get('zoom'),
-            type: e.get('newType') || this.get('type')
-        })
-    });
-};
+    var oldState = this._state;
 
-MapLocation.prototype.get = function (param) {
-    return this._state[param];
-};
-
-MapLocation.prototype.set = function (param, value) {
-    if(value != null) {
-        this._state[param] = value;
+    if(this._silent) {
+        this._silent = false;
     }
-
-    return this;
+    else {
+        this._map.events.fire('locationstatechange', {
+            oldState: oldState,
+            newState: (this._state = new MapLocationState({
+                center: (e.get('newCenter') || oldState.get('center')).map(MapLocationState.toCoords),
+                zoom: e.get('newZoom') >= 0 ? e.get('newZoom') : oldState.get('zoom'),
+                type: e.get('newType') || oldState.get('type')
+            }))
+        });
+    }
 };
 
 MapLocation.prototype.getState = function () {
     return this._state;
 };
 
-MapLocation.prototype.toString = function () {
-    var state = this._state,
+MapLocation.prototype.setState = function (data) {
+    var map = this._map,
+        state = this._state;
+
+    this._silent = true;
+
+    if(data.type === state.get('type')) {
+        map.setCenter(data.center, data.zoom);
+        state
+            .set('center', data.center)
+            .set('zoom', data.zoom);
+    }
+    else {
+        map.setType(data.type);
+        state.set('type', data.type);
+    }
+
+    return this;
+};
+
+function MapLocationState(data) {
+    this._data = data;
+}
+
+MapLocationState.prototype.get = function (param) {
+    return this._data[param];
+};
+
+MapLocationState.prototype.set = function (param, value) {
+    if(value != null) {
+        this._data[param] = value;
+    }
+
+    return this;
+};
+
+MapLocationState.prototype.getData = function () {
+    return this._data;
+};
+
+MapLocationState.prototype.toString = function () {
+    var data = this._data,
         params = [];
 
-    for(var param in state) {
-        params.push(encodeURI(param) + '=' + encodeURIComponent(state[param]));
+    for(var param in data) {
+        params.push(encodeURI(param) + '=' + encodeURIComponent(data[param]));
     }
 
     return params.join('&');
 };
 
-MapLocation.fromString = function (location) {
+MapLocationState.fromString = function (location) {
     var params = {};
 
     location.replace(/[^?&#]+(?=&|$)/g, function (s) {
@@ -55,13 +88,13 @@ MapLocation.fromString = function (location) {
         params[decodeURI(param[0])] = decodeURIComponent(param[1]);
     });
 
-    return new MapLocation({
-        center: (params.center || '0,0').split(',').map(MapLocation.toCoords),
-        zoom: Number(params.zoom || '0'),
+    return new MapLocationState({
+        center: params.center.split(',').map(MapLocationState.toCoords),
+        zoom: Number(params.zoom),
         type: params.type || 'yandex#map'
     });
 };
 
-MapLocation.toCoords = function (i) {
+MapLocationState.toCoords = function (i) {
     return Number(i).toFixed(6);
 };
