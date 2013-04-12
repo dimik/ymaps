a9ec10d7fbeefefbd430f9eee2b19fbef9095fd4
dimik <dimik@yandex.ru>
2013-04-12 18:24:14 +0700

    документация, - this.state

--- examples/location-tool/cross-control.js
+++ examples/location-tool/cross-control.js
@@ -1,17 +1,40 @@
-function CrossControl() {
+/**
+ * Класс контрола "центр карты".
+ * @class
+ * @name CrossControl
+ */
+function CrossControl(options) {
     this.events = new ymaps.event.Manager();
     this.options = new ymaps.option.Manager();
-    this.state = new ymaps.data.Manager();
 }
 
 ymaps.ready(function () {
+    /**
+     * Макет контрола.
+     * @see http://api.yandex.ru/maps/doc/jsapi/2.x/ref/reference/templateLayoutFactory.xml
+     * @class
+     * @name CrossControl.Layout
+     */
     CrossControl.Layout = ymaps.templateLayoutFactory.createClass(
         '<div class="cross-control" style="right:$[options.position.right]px; top:$[options.position.top]px;"></div>'
     );
 });
 
+/**
+ * @lends CrossControl.prototype
+ */
 CrossControl.prototype = {
+    /**
+     * @constructor
+     */
     constructor: CrossControl,
+    /**
+     * Устанавливает родительский объект.
+     * @function
+     * @name CrossControl.setParent
+     * @param {IControlParent} parent Родительский объект.
+     * @returns {CrossControl} Возвращает ссылку на себя.
+     */
     setParent: function (parent) {
         this.parent = parent;
 
@@ -20,23 +43,43 @@ CrossControl.prototype = {
 
             this._setPosition(map.container.getSize());
             map.container.events.add('sizechange', this._setPosition, this);
-            // передаем в макет контрола данные о его опциях.
+            /**
+             * Передаем в макет контрола данные о его опциях.
+             * @see http://api.yandex.ru/maps/doc/jsapi/2.x/ref/reference/ILayout.xml#constructor-summary
+             */
             this.layout = new this.constructor.Layout({ options: this.options });
-            // контрол будет добавляться в pane событий.
+            /**
+             * Контрол будет добавляться в pane событий, чтобы исключить интерактивность.
+             * @see http://api.yandex.ru/maps/doc/jsapi/2.x/ref/reference/ILayout.xml#setParentElement
+             */
             this.layout.setParentElement(map.panes.get('events').getElement());
         }
         else {
             this.layout.setParentElement(null);
         }
     },
+    /**
+     * Возвращает ссылку на родительский объект.
+     * @see http://api.yandex.ru/maps/doc/jsapi/2.x/ref/reference/IControl.xml#getParent
+     * @function
+     * @name CrossControl.getParent
+     * @returns {IControlParent} Ссылка на родительский объект.
+     */
+    getParent: function () {
+        return this.parent;
+    },
+    /**
+     * Устанавливает контролу опцию "position".
+     * @function
+     * @private
+     * @name CrossControl._setPosition
+     * @param {Array} size Размер контейнера карты.
+     */
     _setPosition: function (size) {
         // -8, так как картинка 16х16
         this.options.set('position', {
             top: size[1] / 2 - 8,
             right: size[0] / 2 - 8
         });
-    },
-    getParent: function () {
-        return this.parent;
     }
 };
