2c51ad0e4c584118a0bccc33abf0433731650721
dimik <dimik@yandex.ru>
2013-06-21 11:00:01 +0200

    js-doc

--- examples/regions/mask-overlay.js
+++ examples/regions/mask-overlay.js
@@ -1,16 +1,43 @@
+/**
+ * Класс оверлея маски.
+ * @class
+ * @name MaskOverlay
+ * @param [ymaps.geometry.pixel.Polygon] geometry Пиксельная геометкрия полигона.
+ * @param [ymaps.data.Manager] data Менеджер данных.
+ * @param [ymaps.option.Manager] options Менеджер опций.
+ */
 function MaskOverlay(geometry, data, options) {
     MaskOverlay.superclass.constructor.call(this, geometry, data, options);
 }
 
 ymaps.ready(function () {
+    /**
+     * @lends MaskOverlay.prototype
+     */
     ymaps.util.augment(MaskOverlay, ymaps.overlay.staticGraphics.Polygon, {
+        /**
+         * @constructor
+         */
         constructor: MaskOverlay,
+        /**
+         * Перекрываем публичный метод.
+         * @function
+         * @name MaskOverlay.setGeometry
+         * @param [ymaps.geometry.pixel.Polygon] geometry Пиксельная геометрия полигона.
+         */
         setGeometry: function (geometry) {
             MaskOverlay.superclass.setGeometry.call(
                 this,
                 this.getMap() ? this._createGeometry(geometry) : geometry
             );
         },
+        /**
+         * Создание пиксельной геометрии.
+         * @function
+         * @private
+         * @name MaskOverlay._createGeometry
+         * @returns [ymaps.geometry.pixel.Polygon] Пиксельная геометрия полигона.
+         */
         _createGeometry: function (geometry) {
             var lineCoordinates = geometry.getCoordinates().slice(0),
                 map = this.getMap(),
