32ec6845e4c724a0d4462dbe99a2d58447971d19
dimik <dimik@yandex.ru>
2013-08-01 23:33:07 +0700

    FormView, new Model and error messages

--- examples/geocoder-form/search-address.js
+++ examples/geocoder-form/search-address.js
@@ -1,5 +1,6 @@
 function SearchAddress(map, form) {
-    this._formModel = new SearchAddress.FormModel(form);
+    this._model = new SearchAddress.Model();
+    this._formView = new SearchAddress.FormView(form);
     this._mapView = new SearchAddress.MapView(map);
 
     this._attachHandlers();
@@ -8,23 +9,38 @@ function SearchAddress(map, form) {
 SearchAddress.prototype = {
     constructor: SearchAddress,
     _attachHandlers: function () {
-        this._formModel.events
-            .on('searchsuccess', $.proxy(this._onSearchSuccess, this))
-            .on('searcherror', $.proxy(this._onSearchError, this));
+        this._formView.events
+            .on('searchrequest', $.proxy(this._onSearchRequest, this));
     },
     _detachHandlers: function () {
-        this._formModel.events
+        this._formView.events
             .off();
     },
-    _onSearchSuccess: function (e) {
+    _onSearchRequest: function (e) {
+        var promise = this._model.search(e.query);
+
         this._mapView
-            .clear()
-            .render(e.searchresult);
+            .clear();
+
+        promise.then(
+            $.proxy(this._onSearchSuccess, this),
+            $.proxy(this._onSearchError, this)
+        );
+    },
+    _onSearchSuccess: function (result) {
+        if(this._model.getResult()) {
+            this._mapView
+                .render(result);
+        }
+        else {
+            this._formView
+                .showMessage("Ничего не найдено.");
+        }
     },
     _onSearchError: function (e) {
-        if(window.console) {
-            console.log(e.description);
-        }
+        this._formView.showMessage(
+            this._model.getError()
+        );
     },
     getModel: function () {
         return this._formModel;
@@ -38,18 +54,20 @@ SearchAddress.MapView = function (map) {
 
 SearchAddress.MapView.prototype = {
     constructor: SearchAddress.MapView,
-    render: function (data) {
-        var balloonContent = '<p><small>по запросу:</small>&nbsp;<em>' + data.request + '</em></p>' +
-                '<p><small>найдено:</small>&nbsp;<strong>' + data.properties.text + '</strong></p>';
+    render: function (results) {
+        var metaData = results.metaData.geocoder,
+            result = results.geoObjects.get(0),
+            balloonContent = '<p><small>по запросу:</small>&nbsp;<em>' + metaData.request + '</em></p>' +
+                '<p><small>найдено:</small>&nbsp;<strong>' + result.properties.get('text') + '</strong></p>';
 
-        this._point = new ymaps.Placemark(data.coordinates, {
+        this._point = new ymaps.Placemark(result.geometry.getCoordinates(), {
             balloonContentBody: balloonContent
         });
 
         this._map.geoObjects
             .add(this._point);
 
-        this._setMapBounds(data.properties.boundedBy);
+        this._setMapBounds(result.properties.get('boundedBy'));
 
         return this;
     },
@@ -75,35 +93,75 @@ SearchAddress.MapView.prototype = {
     }
 };
 
-SearchAddress.FormModel = function (form) {
+SearchAddress.FormView = function (form) {
     this._form = form;
-    this._request = null;
+    this._controls = form.find('.control-group');
+    this._message = form.find('.help-inline');
+    this._input = form.find('.search-query');
+
     this.events = $({});
 
     this._attachHandlers();
 };
 
-SearchAddress.FormModel.prototype = {
-    constructor: SearchAddress.FormModel,
+SearchAddress.FormView.prototype = {
+    constructor: SearchAddress.FormView,
     _attachHandlers: function () {
         this._form
             .on('submit', $.proxy(this._onFormSubmit, this));
+        this._input
+            .on('keydown', $.proxy(this._onInputChange, this));
     },
     _detachHandlers: function () {
         this._form
             .off("submit");
+        this._input
+            .off();
     },
     _onFormSubmit: function (e) {
         e.preventDefault();
 
-        var input = $(e.target).find('.search-query');
+        var value = this._input.val();
 
-        if(input.val()) {
-            this.search(input.val());
+        if(value) {
+            this.events.trigger($.Event('searchrequest', {
+                query: value
+            }));
+        }
+        else {
+            this.showMessage('Задан пустой поисковый запрос.');
         }
     },
-    search: function (address) {
-        var promise = ymaps.geocode(this._request = address);
+    _onInputChange: function (e) {
+        this.hideMessage();
+    },
+    showMessage: function (text) {
+        this._controls
+            .addClass('error');
+        this._message
+            .removeClass('invisible')
+            .text(text);
+    },
+    hideMessage: function () {
+        this._controls
+            .removeClass('error');
+        this._message
+            .addClass('invisible')
+            .text('');
+    }
+};
+
+SearchAddress.Model = function () {
+    this._result = null;
+    this._error = null;
+};
+
+SearchAddress.Model.prototype = {
+    constructor: SearchAddress.Model,
+    search: function (request) {
+        var promise = ymaps.geocode(request, this.getDefaults());
+
+        this.clear();
 
         promise.then(
             $.proxy(this._onSearchSuccess, this),
@@ -112,22 +170,25 @@ SearchAddress.FormModel.prototype = {
 
         return promise;
     },
+    clear: function () {
+        this._result = null;
+        this._error = null;
+    },
     _onSearchSuccess: function (result) {
-        var firstResult = result.geoObjects.get(0);
-
-        if(firstResult) {
-            this.events.trigger($.Event('searchsuccess', {
-                searchresult: {
-                    request: this._request,
-                    coordinates: firstResult.geometry.getCoordinates(),
-                    properties: firstResult.properties.getAll()
-                }
-            }));
-        }
+        this._result = result.geoObjects.get(0);
     },
     _onSearchFailed: function (error) {
-        this.events.trigger($.Event('searcherror', {
-            description: error
-        }));
+        this._error = error;
+    },
+    getDefaults: function () {
+        return {
+            results: 1
+        };
+    },
+    getResult: function () {
+        return this._result;
+    },
+    getError: function () {
+        return this._error;
     }
 };
