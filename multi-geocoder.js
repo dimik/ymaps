cd0f68c22b28f9acbd4fa62196af79fa80a6b95d
dimik <dimik@yandex.ru>
2013-10-02 11:39:15 +0700

    change GeoObjectArray to ListCollection

--- multi-geocoder.js
+++ multi-geocoder.js
@@ -4,17 +4,19 @@
  * Аналогичная разработка для первой версии АПИ.
  * @see http://api.yandex.ru/maps/doc/jsapi/1.x/examples/multiplygeocoding.html
  * @example
- * var multiGeocoder = new MultiGeocoder({ boundedBy : map.getBounds() });
- * multiGeocoder
- *     .geocode(['Москва, Льва Толстого 16', [55.7, 37.5], 'Санкт-Петербург'])
- *         .then(
- *             function (res) {
- *                 map.geoObjects.add(res.geoObjects);
- *             },
- *             function (err) {
- *                 console.log(err);
- *             }
- *         );
+
+   var multiGeocoder = new MultiGeocoder({ boundedBy : map.getBounds() });
+
+   multiGeocoder
+       .geocode(['Москва, Льва Толстого 16', [55.7, 37.5], 'Санкт-Петербург'])
+           .then(
+               function (res) {
+                   map.geoObjects.add(res.geoObjects);
+               },
+               function (err) {
+                   console.log(err);
+               }
+           );
  */
 
 /**
@@ -40,20 +42,18 @@ function MultiGeocoder(options) {
  */
 MultiGeocoder.prototype.geocode = function (requests, options) {
     var self = this,
-        opts = ymaps.util.extend({}, self._options, options),
         size = requests.length,
         promise = new ymaps.util.Promise(),
-        result = [],
-        geoObjects = new ymaps.GeoObjectArray();
+        geoObjects = new ListCollection();
 
     requests.forEach(function (request, index) {
-        ymaps.geocode(request, opts)
+        ymaps.geocode(request, ymaps.util.extend({}, self._options, options))
             .then(
                 function (response) {
                     var geoObject = response.geoObjects.get(0);
 
-                    geoObject && (result[index] = geoObject);
-                    --size || (result.forEach(geoObjects.add, geoObjects), promise.resolve({ geoObjects : geoObjects }));
+                    geoObject && geoObjects.add(geoObject, index);
+                    --size || promise.resolve({ geoObjects : geoObjects });
                 },
                 function (err) {
                     promise.reject(err);
@@ -63,4 +63,3 @@ MultiGeocoder.prototype.geocode = function (requests, options) {
 
     return promise;
 };
-
