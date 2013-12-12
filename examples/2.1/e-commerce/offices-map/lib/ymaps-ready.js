8aa089d6af6470edbcd2054fc5c4a58be8d0a671
dimik <dimik@yandex.ru>
2013-12-12 16:52:00 +0700

    удалил коммент

--- examples/2.1/e-commerce/offices-map/lib/ymaps-ready.js
+++ examples/2.1/e-commerce/offices-map/lib/ymaps-ready.js
@@ -1,7 +1,3 @@
-/**
- * RequireJS plugin
- * Loads Yandex Maps API correctly by calling ymaps.ready
- */
 define({
     load: function (name, require, load, config) {
         require([name], function (ymaps) {
