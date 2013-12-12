7bcf41ee51b908e4bdc38e7183535ab3b6191e55
dimik <dimik@yandex.ru>
2013-12-12 15:23:48 +0700

    change coordinates order

--- examples/2.1/e-commerce/offices-map/app.js
+++ examples/2.1/e-commerce/offices-map/app.js
@@ -1,7 +1,7 @@
 requirejs.config({
     baseUrl: 'lib',
     paths: {
-        ymaps: '//api-maps.yandex.ru/2.1-dev/?lang=ru-RU&load=package.full',
+        ymaps: '//api-maps.yandex.ru/2.1-dev/?lang=ru-RU&load=package.full&coordorder=longlat',
         jquery: '//yandex.st/jquery/2.0.3/jquery.min',
         ready: 'ymaps-ready'
     },
