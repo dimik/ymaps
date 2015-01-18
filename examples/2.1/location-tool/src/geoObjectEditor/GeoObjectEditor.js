8a163daadafb789262835c21090c695d7d409e88
dimik <dimik@yandex.ru>
2015-01-18 13:25:30 +0700

    refactoring

--- examples/2.1/location-tool/src/geoObjectEditor/GeoObjectEditor.js
+++ examples/2.1/location-tool/src/geoObjectEditor/GeoObjectEditor.js
@@ -18,7 +18,8 @@ ym.modules.define('GeoObjectEditor', [
         map.geoObjects.add(this);
 
         this._mapCursor = null;
-        this._currentObject = null;
+        this._activeObject = null;
+        this._activeObjectEditorMonitor = null;
 
         this._drawingControl = new DrawingControl();
         map.controls.add(this._drawingControl);
@@ -31,6 +32,8 @@ ym.modules.define('GeoObjectEditor', [
     }, GeoObjectCollection, {
         _setupListeners: function () {
             this.events
+                .add('editorstatechange', this._onChildEditorStateChange, this)
+                .add('remove', this._onChildRemove, this)
                 .add('mapchange', this._onMapChange, this);
             this._drawingControl.events
                 .add('select', this._onControlSelect, this)
@@ -40,6 +43,22 @@ ym.modules.define('GeoObjectEditor', [
             this._stateMonitor
                 .add('drawing', this._onDrawingChange, this)
                 .add('editing', this._onEditingChange, this);
+        },
+        _onChildEditorStateChange: function (e) {
+            var geoObject = e.get('target');
+
+            if(this._activeObject !== geoObject) {
+                this.unsetActiveObject();
+                this.setActiveObject(geoObject);
+                this._onActiveObjectEditingChange(geoObject.editor.state.get('editing'));
+            }
+        },
+        _onChildRemove: function (e) {
+            var geoObject = e.get('child');
+
+            if(geoObject === this._activeObject) {
+                this.unsetActiveObject();
+            }
         },
         _onDrawingChange: function (isDrawing) {
             if(isDrawing) {
@@ -52,29 +71,12 @@ ym.modules.define('GeoObjectEditor', [
             }
         },
         _onEditingChange: function (isEditing) {
-            if(isEditing) {
-                this._listenRightClick();
-            }
-            else {
-                this._unlistenRightClick();
-            }
         },
         _listenMapClick: function () {
             this._map.events.add('click', this._onMapClick, this);
         },
         _unlistenMapClick: function () {
             this._map.events.remove('click', this._onMapClick, this);
-        },
-        _listenRightClick: function (e) {
-            this.events.add('contextmenu', this._onRightClick, this);
-        },
-        _unlistenRightClick: function (e) {
-            this.events.remove('contextmenu', this._onRightClick, this);
-        },
-        _onRightClick: function (e) {
-            this._openEditMenu(e.get('coords'));
-        },
-        _openEditMenu: function () {
         },
         _addMapCursor: function () {
             this._mapCursor = this._map.cursors.push('crosshair');
@@ -94,17 +96,67 @@ ym.modules.define('GeoObjectEditor', [
         stopEditing: function () {
             this.state.set('editing', false);
         },
-        _onMapClick: function (e) {
-            this.state.set('drawing', false);
+        setActiveObject: function (geoObject) {
+            if(geoObject !== this._activeObject) {
+                this._activeObject = geoObject;
+                this._setupActiveObjectEditorMonitor();
+            }
+        },
+        unsetActiveObject: function () {
+            var activeObject = this._activeObject;
 
-            var coordinates = e.get('coords');
-            var geoObject = this._currentObject = this._createGeoObject(coordinates);
-            this.add(geoObject);
-            geoObject.editor.startDrawing();
-            geoObject.events.once('editorstatechange', function () {
-                geoObject.editor.stopEditing();
+            if(activeObject) {
+                this._clearActiveObjectEditorMonitor();
+                if(activeObject.editor.state.get('drawing')) {
+                    activeObject.editor.stopDrawing();
+                }
+                if(activeObject.editor.state.get('editing')) {
+                    activeObject.editor.stopEditing();
+                }
+                if(activeObject.balloon.isOpen()) {
+                    activeObject.balloon.close();
+                }
+                activeObject.state.set('editing', false);
+            }
+        },
+        _setupActiveObjectEditorMonitor: function () {
+            var activeObject = this._activeObject;
+
+            if(activeObject) {
+                var monitor = this._activeObjectEditorMonitor = new Monitor(activeObject.editor.state);
+                monitor.add('drawing', this._onActiveObjectDrawingChange, this);
+                monitor.add('editing', this._onActiveObjectEditingChange, this);
+            }
+        },
+        _clearActiveObjectEditorMonitor: function () {
+            if(this._activeObjectEditorMonitor) {
+                this._activeObjectEditorMonitor.removeAll();
+            }
+
+            this._activeObjectEditorMonitor = null;
+        },
+        _onActiveObjectDrawingChange: function (drawing) {
+            var activeObject = this._activeObject;
+
+            if(!drawing) {
+                activeObject.editor.stopEditing();
+                activeObject.balloon.open();
                 this._deselectDrawingControl();
-            }, this);
+            }
+        },
+        _onActiveObjectEditingChange: function (editing) {
+            var activeObject = this._activeObject;
+
+            activeObject.options.set('draggable', editing);
+            activeObject.state.set('editing', editing);
+        },
+        _onMapClick: function (e) {
+            var geoObject = this._createGeoObject(e.get('coords'));
+
+            this.add(geoObject);
+            this.setActiveObject(geoObject);
+            geoObject.editor.startDrawing();
+            this.state.set('drawing', false);
         },
         _onControlSelect: function (e) {
             var target = e.get('target');
@@ -113,6 +165,7 @@ ym.modules.define('GeoObjectEditor', [
                 geometryType: target.data.get('geometryType'),
                 drawing: true
             });
+            this.unsetActiveObject();
         },
         _onControlDeselect: function (e) {
             var target = e.get('target');
