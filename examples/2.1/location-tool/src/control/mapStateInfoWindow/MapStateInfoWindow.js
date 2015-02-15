d4599bda428889b4f80abed33fe7e63df696588e
dimik <dimik@yandex.ru>
2015-02-15 22:39:00 +0700

    submit map stet

--- examples/2.1/location-tool/src/control/mapStateInfoWindow/MapStateInfoWindow.js
+++ examples/2.1/location-tool/src/control/mapStateInfoWindow/MapStateInfoWindow.js
@@ -2,25 +2,58 @@ ym.modules.define('control.MapStateInfoWindow', [
     'util.defineClass',
     'collection.Item',
     'data.Manager',
+    'Monitor',
     'layout.MapStateInfoWindowLayout'
-], function (provide, defineClass, CollectionItem, DataManager, layoutStorage) {
+], function (provide, defineClass, CollectionItem, DataManager, Monitor, layoutStorage) {
     var MapStateInfoWindow = defineClass(function (options) {
         MapStateInfoWindow.superclass.constructor.call(this, options);
 
         this.data = new DataManager();
+        this.state = new DataManager();
+        this._stateMonitor = new Monitor(this.state);
     }, CollectionItem, {
         onAddToMap: function (map) {
             MapStateInfoWindow.superclass.onAddToMap.call(this, map);
 
+            this.state.set({
+                mapCenter: map.getCenter(),
+                mapZoom: map.getZoom(),
+                mapBounds: map.getBounds()
+            });
+            this._setupStateMonitor();
             this.getParent().getChildElement(this).then(this._onChildElement, this);
         },
         onRemoveFromMap: function (oldMap) {
+            this._clearStateMonitor();
+
             MapStateInfoWindow.superclass.onRemoveFromMap.call(this, oldMap);
+        },
+        _setupStateMonitor: function () {
+            this._stateMonitor
+                .add('mapCenter', this._onMapCenterChange, this)
+                .add('mapZoom', this._onMapZoomChange, this)
+                .add('mapBounds', this._onMapBoundsChange, this)
+        },
+        _clearStateMonitor: function () {
+            this._stateMonitor.removeAll();
+        },
+        _onMapCenterChange: function (newCenter) {
+            this.getMap().setCenter(newCenter);
+        },
+        _onMapZoomChange: function (newZoom) {
+            this.getMap().setZoom(newZoom, {
+                checkZoomRange: true
+            });
+        },
+        _onMapBoundsChange: function (newBounds) {
+            this.getMap().setBounds(newBounds, {
+                checkZoomRange: true
+            });
         },
         _onChildElement: function (parentDomContainer) {
             var Layout = layoutStorage.get('mapstate#window');
 
-            this.layout = new Layout({ data: this.data, options: this.options });
+            this.layout = new Layout({ control: this, data: this.data, options: this.options });
             /**
              * Контрол будет добавляться в pane событий, чтобы исключить интерактивность.
              * @see http://api.yandex.ru/maps/doc/jsapi/2.x/ref/reference/ILayout.xml#setParentElement
--- examples/2.1/location-tool/src/mapStateInfo/layout.js
+++ examples/2.1/location-tool/src/mapStateInfo/layout.js
@@ -31,10 +31,77 @@ ym.modules.define('layout.MapStateInfoWindowLayout', [
                         '<input type="text" id="mapBounds" class="input-mini" value="{{ data.mapBounds|json }}">',
                     '</div>',
                 '</div>',
+                '<input type="submit" style="position: absolute; left: -9999px"/>',
             '</form>',
             '</div>',
         '</div>'
-    ].join(''));
+    ].join(''), {
+        build: function () {
+            MapStateInfoWindowLayout.superclass.build.call(this);
+
+            this._setupListeners();
+        },
+        clear: function () {
+            MapStateInfoWindowLayout.superclass.clear.call(this);
+        },
+        _setupListeners: function () {
+            var element = this.getElement();
+
+            jQuery('form', element).on('submit', jQuery.proxy(this._onStateSubmit, this));
+
+            /*
+            jQuery(element)
+                .on('keyup', '#mapCenter', jQuery.debounce(this._onMapCenterChange, 1000, this))
+                .on('keyup', '#mapZoom', jQuery.debounce(this._onMapZoomChange, 1000, this))
+                .on('keyup', '#mapBounds', jQuery.debounce(this._onMapBoundsChange, 1000, this));
+                */
+        },
+        _clearListeners: function () {
+        },
+        _onStateSubmit: function (e) {
+            e.preventDefault();
+
+            var element = this.getElement();
+            var mapCenter = jQuery('#mapCenter', element).val();
+            var mapZoom = jQuery('#mapZoom', element).val();
+            var mapBounds = jQuery('#mapBounds', element).val();
+            var newState = {};
+            try {
+                var center = mapCenter.match(/([\d.]+),([\d.]+)/).slice(1, 3).map(Number);
+
+                if(center.every(isFinite)) {
+                    newState['mapCenter'] = center;
+                }
+            }
+            catch(e) {}
+            var zoom = Number.parseFloat(mapZoom);
+            if(zoom >= 0) {
+                newState['mapZoom'] = zoom;
+            }
+
+            this.getData().control.state.set(newState);
+        },
+        _onMapCenterChange: function (e) {
+            try {
+                var coords = e.target.value.match(/([\d.]+),([\d.]+)/).slice(1, 3).map(Number);
+
+                if(Number.isFinite(coords[0]) && Number.isFinite(coords[1])) {
+                    this.getData().control.state.set('mapCenter', coords);
+                }
+            }
+            catch(e) {}
+        },
+        _onMapZoomChange: function (e) {
+            var zoom = Number.parseFloat(e.target.value);
+
+            if(zoom >= 0) {
+                this.getData().control.state.set('mapZoom', zoom);
+            }
+        },
+        _onMapBoundsChange: function (e) {
+
+        }
+    });
 
     layoutStorage.add('mapstate#window', MapStateInfoWindowLayout);
 
