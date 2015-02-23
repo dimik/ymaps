fc3c6d228c85f8608792e8e470c4d94f4f0b194e
dimik <dimik@yandex.ru>
2015-02-23 15:50:19 +0700

    using balloonOffset property

--- examples/2.1/rose-clusterer/src/roseClusterer/RoseClusterer.js
+++ examples/2.1/rose-clusterer/src/roseClusterer/RoseClusterer.js
@@ -15,9 +15,9 @@ var RoseClusterer = defineClass(
         }, options));
     }, Clusterer, {
         createCluster: function (center, geoObjects) {
-            var map = this.getMap(),
-                zoom = map.getZoom(),
-                maxZoom = map.zoomRange.getCurrent()[1];
+            var map = this.getMap();
+            var zoom = map.getZoom();
+            var maxZoom = map.zoomRange.getCurrent()[1];
 
             if(zoom >= maxZoom) {
                  return this._createCollection(center, geoObjects);
@@ -38,37 +38,39 @@ var RoseClusterer = defineClass(
             return collection;
         },
         _onCollectionMapChange: function (e) {
-            var collection = e.get('target'),
-                center = collection.properties.get('center'),
-                num = collection.getLength(),
-                radius = this.options.get('roseRadius');
+            var collection = e.get('target');
+            var map = this.getMap();
+            var projection = map.options.get('projection');
+            var center = collection.properties.get('center');
+            var zoom = map.getZoom();
+            var pixelCenter = projection.toGlobalPixels(center, zoom);
+            var radius = this.options.get('roseRadius');
+            var getGeoObjectOffset = function (geoObject) {
+                var pixelCoordinates = projection.toGlobalPixels(
+                    geoObject.geometry.getCoordinates(),
+                    zoom
+                );
+
+                return [
+                    pixelCenter[0] - pixelCoordinates[0],
+                    pixelCenter[1] - pixelCoordinates[1]
+                ];
+            };
 
             collection.each(function (geoObject, index) {
-                var angle = Math.PI * 2 / num * (num - index),
-                    offset = this._getGeoObjectOffset(center, geoObject);
+                var num = collection.getLength();
+                var angle = Math.PI * 2 / num * (num - index);
+                var geoObjectOffset = getGeoObjectOffset(geoObject);
+                var offset = [
+                    geoObjectOffset[0] + Math.sin(angle) * radius,
+                    geoObjectOffset[1] + Math.cos(angle) * radius
+                ];
 
-                geoObject.getOverlay()
-                    .then(function (overlay) {
-                        overlay.options.set('offset', [
-                            offset[0] + Math.sin(angle) * radius,
-                            offset[1] + Math.cos(angle) * radius
-                        ]);
-                    });
-                console.log(geoObject.balloon.getOverlay() == null && '????????');
-            }, this);
-        },
-        _getGeoObjectOffset: function (center, geoObject) {
-            var map = this.getMap(),
-                zoom = map.getZoom(),
-                projection = map.options.get('projection'),
-                pixelCenter = projection.toGlobalPixels(center, zoom),
-                coordinates = geoObject.geometry.getCoordinates(),
-                pixelCoordinates = projection.toGlobalPixels(coordinates, zoom);
-
-            return [
-                pixelCenter[0] - pixelCoordinates[0],
-                pixelCenter[1] - pixelCoordinates[1]
-            ];
+                geoObject.options.set({
+                    iconOffset: offset,
+                    balloonOffset: offset
+                });
+            });
         }
     });
 
