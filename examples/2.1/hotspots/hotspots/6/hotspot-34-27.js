e81a6e84db1b8169e7dc2327ec85cc7a2b0f02df
dimik <dimik@yandex.ru>
2015-03-21 01:03:35 +0700

    hotspots example

---
+++ examples/2.1/hotspots/hotspots/6/hotspot-34-27.js
@@ -0,0 +1,20 @@
+hotspot_callback({
+    "data": {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "hintContent": "Содержимое текстовой подсказки.",
+                "HotspotMetaData": {
+                    "id": 'BY',
+                    "zIndex": 10,
+                    "RenderedGeometry": {
+                        "type": "Rectangle",
+                        // координаты передаются в пиксельных координатах, отсчитываемых от верхнего левого угла тайла.
+                        "coordinates": [ [129, 71], [ 256, 14 ] ]
+                    }
+                }
+            }
+        }]
+    }
+});
