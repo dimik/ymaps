1cd16edc13f8542f39ad5a5856bf38a07dd0eb1d
dimik <dimik@yandex.ru>
2015-03-23 16:05:26 +0700

    add some hotspot data

---
+++ examples/2.1/hotspots/hotspots/6/hotspot-25-34.js
@@ -0,0 +1,19 @@
+hotspot_callback({
+    "data": {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {
+                "hintContent": "сериалы бразильские список<br/>футбол чемпионат бразилии серия а результаты игр вчера<br/>проспект бразилии 128 серия",
+                "HotspotMetaData": {
+                    "id": "BR",
+                    "zIndex": 10,
+                    "RenderedGeometry": {
+                        "type": "Rectangle",
+                        "coordinates": [ [33, 110], [256, 246] ]
+                    }
+                }
+            }
+        }]
+    }
+});
